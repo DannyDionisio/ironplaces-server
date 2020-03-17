1b9b88097946072db2182e4aaa92edea9f100a6f
Daniela Dionisio <dannyy.dionisio@gmail.com>
2020-03-17 21:07:11 +0000

    config mongo  to heroku

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ admin.initializeApp({
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
